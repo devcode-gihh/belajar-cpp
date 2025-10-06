992a296979248288815a0a3563a8450077693ec8
devcode-gihh <mochamadsugianasaepul@gmail.com>
2025-10-06 08:33:57 +0700

    belajar cpp - deklarasi & cin

---
+++ EPISODE 08 - DEKLARASI & CIN/deklarasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  a = 35;
+  cout << a << endl;
+
+  int b;
+  cout << "Masukan nilai : ";
+  cin >> b;
+  cout << "Nilai yang anda masukan adalah : ";
+  cout << b << endl;
+
+  cin.get();
+  return 0;
+}
