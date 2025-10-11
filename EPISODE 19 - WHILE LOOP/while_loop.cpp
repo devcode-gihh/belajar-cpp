b2ce6b56fdf090fd7b0daa8591d8b11817cabefb
devcode-gihh <mochamadsugianasaepul@gmail.com>
2025-10-11 20:54:52 +0700

    belajar cpp - while loop

---
+++ EPISODE 19 - WHILE LOOP/while_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 5;
+
+  while (a <= 10) {
+    cout << "Horee! " << a << endl;
+    a += 1;
+  }
+
+  cout << "Program while loop selesai!" << endl;
+  
+
+  cin.get();
+  return 0;
+}
